085e608b2da524cd6f1dee6c81320a9c60adc06a
Nicolò Sangiorgio <n.sangiorgio@hotmail.it>
2021-12-17 13:07:56 +0100

    primi passi

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
+});
+
+Route::get('/my-first-page', function () {
+  return view('my_first_page');
 });
